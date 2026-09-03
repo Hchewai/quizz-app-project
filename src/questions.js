61b226e54656e8772f65761eef9f221d97b98097
Hchewai <20127442@tafe.wa.edu.au>
2026-09-03 18:05:37 +0800

    created questions structure, wip

--- src/questions.js
+++ src/questions.js
@@ -1,6 +1,10 @@
 //where the data for the questions and answers will be in an object
 const QuizQuestions = {
-  firstQuestion: "What has four legs and flies?",
+  firstQuestion: {
+    question: "What has four legs and flies?",
+    answers: ["A Dog", "Two Birds", "Mantisflies"],
+    correctAnswer: "Mantisflies",
+  },
 };
 
 export default QuizQuestions;
