7f2009819ce2b6ff78512d0a6fd990f07de49076
Hchewai <20127442@tafe.wa.edu.au>
2026-09-04 20:15:15 +0800

    added the rest of the questions and created an empty question card function

--- src/questions.js
+++ src/questions.js
@@ -5,6 +5,17 @@ const QuizQuestions = {
     answers: ["A Dog", "Two Birds", "Mantisflies"],
     correctAnswer: "Mantisflies",
   },
+  secondQuestion: {
+    question: "What colour is the sky?",
+    answers: ["Green", "Blue", "Purple"],
+    correctAnswer: "Blue",
+  },
+  thirdQuestion: {
+    question: "What has to be broken before you can use it?",
+    answers: ["Egg", "Leg", "Table"],
+    correctAnswer: "Egg",
+  },
 };
 
+function QuestionCard() {}
 export default QuizQuestions;
