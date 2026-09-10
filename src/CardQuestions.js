0c0db238766dc3c48efedace0d73e570c8c24a24
Hchewai <20127442@tafe.wa.edu.au>
2026-09-10 19:03:08 +0800

    created mapped answers

--- src/CardQuestions.js
+++ src/CardQuestions.js
@@ -1,14 +1,20 @@
 import QuizQuestions from "./data/questions";
 function QuestionCard() {
   const mappedQuestions = QuizQuestions.map((QuizQuestion) => (
-    <li key={QuizQuestion.id}>{QuizQuestion.question}</li>
+    <li key={QuizQuestion.id}>
+      {" "}
+      {QuizQuestion.question}
+      <ul>
+        {QuizQuestion.answers.map((answer) => (
+          <li key={answer}>{answer}</li>
+        ))}
+      </ul>
+    </li>
   ));
-
   return (
     <div className="question-card">
       <ul>{mappedQuestions}</ul>
     </div>
   );
 }
-
 export default QuestionCard;
