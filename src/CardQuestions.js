3ddc1594be26971434971bca75e9a77c0eaefb71
Hchewai <20127442@tafe.wa.edu.au>
2026-09-16 17:06:11 +0800

    fix: made handleNextQuestion function neater with ternary

--- src/CardQuestions.js
+++ src/CardQuestions.js
@@ -8,31 +8,20 @@ function QuestionCard() {
 
   // For determining what question the user is on
   const handleNextQuestion = () => {
-    if (chosenAnswer === null) {
-      alert("Please select an answer");
-    } else {
-      setCurrentQuestion((currentQuestion) => currentQuestion + 1);
-    }
+ 
+ chosenAnswer === null ? 
+ alert("Please Select an answer") : 
+ setCurrentQuestion(currentQuestion => currentQuestion + 1); 
   };
 
-<<<<<<< HEAD
- chosenAnswer === null ? alert("Please Select an answer") : currentQuestion + 1; 
-=======
-  // For determining if the answer is right
-  const handleAnswerClick = () => {};
->>>>>>> 6639a7cbbdca0491b3b3a0f2c120b1cd2ae45156
+  const handleChosenAnswer = () =>{
+
+  }
 
   return (
     <div className="question-card">
-<<<<<<< HEAD
       <ul>{QuizQuestions[currentQuestion].question}</ul>
       <button className="next-button" onClick={handleNextQuestion}>Next Question</button>
-=======
-      <ul></ul>
-      <button className="next-button" onClick={handleNextQuestion}>
-        Next Question
-      </button>
->>>>>>> 6639a7cbbdca0491b3b3a0f2c120b1cd2ae45156
     </div>
   );
 }
