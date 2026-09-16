6496d639bd448fd35ca54d02f08e66ab9a724603
Hchewai <20127442@tafe.wa.edu.au>
2026-09-16 18:38:35 +0800

    fix: created answer button map, added handleChosenAnswer logic, added arrow function to onClick for calling handleChosenAnswer
    basic logic works

--- src/CardQuestions.js
+++ src/CardQuestions.js
@@ -14,13 +14,39 @@ function QuestionCard() {
  setCurrentQuestion(currentQuestion => currentQuestion + 1); 
   };
 
-  const handleChosenAnswer = () =>{
+  const handleChosenAnswer = (currentAnswer) =>{
+   if(currentAnswer === QuizQuestions[currentQuestion].correctAnswer) {
+    setChosenAnswer(currentAnswer)
+    console.log("Correct")
+   }   
+   else{
+    console.log("Wrong")
+   }
 
   }
+
+  // creating the answer buttons/options
+    const answerButtons = () => {
+      return(
+      QuizQuestions[currentQuestion].answers.map(currentAnswer => 
+        
+          <li key={currentAnswer}>
+            <button onClick={() => handleChosenAnswer(currentAnswer)}>{currentAnswer}</button>
+          </li>
+        
+      //return something
+      )
+      )
+}
 
   return (
     <div className="question-card">
       <ul>{QuizQuestions[currentQuestion].question}</ul>
+
+      <ul>
+ {answerButtons()}
+</ul>
+
       <button className="next-button" onClick={handleNextQuestion}>Next Question</button>
     </div>
   );
